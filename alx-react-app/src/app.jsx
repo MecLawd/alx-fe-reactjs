899aabe1c4b6e25ff90a246a99c94cf8020a4dde
Saad <clashcrap23@gmail.com>
2024-08-11 16:52:46 +0100

    edited some errors

--- alx-react-app/src/app.jsx
+++ alx-react-app/src/app.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
+import Header from './components/Header' 
 import Footer from './components/Footer'
-import MainContent from './components/MainContent'
+import MainContent from './components/components/MainContent'
 import UserProfile from './components/UserProfile'
 
 function App() {
